0a8e0472e4b910746de222dc72ed078f8fd9da5b
Helder <he7d3r@gmail.com>
2012-07-01 15:46:37 +0000

    Improve the tests, add a closure and validate with JSLint

--- src/ShortDiff.js
+++ src/ShortDiff.js
@@ -6,22 +6,37 @@
  * Due to rewrite rules may not work by default on wikis outside Wikimedia.
  *
  * @source: //meta.wikimedia.org/wiki/MediaWiki:Gadget-ShortDiff.js
- * @author: Krinkle
- * @revision: 2
+ * @author: Krinkle, [[User:Helder.wiki]]
+ * @revision: 3
+ * @tracking: [[Special:GlobalUsage/User:Helder.wiki/Tools/ShortDiff.js]] ([[File:User:Helder.wiki/Tools/ShortDiff.js]])
  */
-// jQuery document ready
+/*jslint browser: true, white: true*/
+/*global jQuery, mediaWiki */
+( function ( $, mw /* , undefined */ ) {
+'use strict';
+
 $(function () {
 	$('a').live('click', function () {
-		var href = $(this).attr('href');
-		var diffVal = mw.util.getParamValue('diff', href);
-		if (!href || !diffVal) {
+		var	newHref, oldidVal,
+			href = $(this).attr('href'),
+			diffVal = mw.util.getParamValue('diff', href);
+		if ( !href
+			|| $.inArray( diffVal, [undefined, null, '', 0, '0', 'cur'] ) !== -1
+			|| $(this).parent().attr( 'id' ) === 't-permalink'
+			|| ( href.indexOf( location.host ) === -1
+				&& href.indexOf('/w/index.php') !== 0
+				&& href.indexOf('/wiki/') !== 0
+			)
+		) {
 			return;
 		}
-		var newHref = mw.config.get('wgScript') + '?diff=' + diffVal;
-		var oldidVal = mw.util.getParamValue('oldid', href);
+		newHref = mw.config.get('wgScript') + '?diff=' + diffVal;
+		oldidVal = mw.util.getParamValue('oldid', href);
 		if (oldidVal) {
 			newHref += '&oldid=' + oldidVal;
 		}
 		$(this).attr('href', newHref);
 	});
 });
+
+}( jQuery, mediaWiki ) );
