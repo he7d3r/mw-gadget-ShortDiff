7088de4369dc79b97d5504443c642c71cc1720cc
Helder <he7d3r@gmail.com>
2014-09-27 20:49:02 -0300

    Apply MediaWiki Coding Conventions
    Validated with jscs 1.6.2, using preset "wikimedia"

--- src/ShortDiff.js
+++ src/ShortDiff.js
@@ -1,6 +1,6 @@
 /**
  * ShortDiff-link
- * 
+ *
  * When clicking a diff-link shorten it to:
  * http://wiki.org/w/index.php?diff=1[&oldid=1]
  * Due to rewrite rules may not work by default on wikis outside Wikimedia.
@@ -11,31 +11,31 @@
  * @license: CC BY-SA 3.0 <https://creativecommons.org/licenses/by-sa/3.0/>
  * @revision: 4
  */
-( function ( $, mw ) {
-'use strict';
+( function ( mw, $ ) {
+	'use strict';
 
-$( function () {
-	$( document ).on( 'click', 'a', function () {
-		var	newHref, oldidVal,
-			href = $(this).attr('href'),
-			diffVal = mw.util.getParamValue('diff', href);
-		if ( !href
-			|| $.inArray( diffVal, [ undefined, null, '', 0, '0', 'cur' ] ) !== -1
-			|| $(this).parent().attr( 'id' ) === 't-permalink'
-			|| ( href.indexOf( location.host ) === -1
-				&& href.indexOf( '/w/index.php' ) !== 0
-				&& href.indexOf( '/wiki/' ) !== 0
-			)
-		) {
-			return;
-		}
-		newHref = mw.config.get( 'wgScript' ) + '?diff=' + diffVal;
-		oldidVal = mw.util.getParamValue( 'oldid', href );
-		if (oldidVal) {
-			newHref += '&oldid=' + oldidVal;
-		}
-		$( this ).attr( 'href', newHref );
+	$( function () {
+		$( document ).on( 'click', 'a', function () {
+			var	newHref, oldidVal,
+				href = $(this).attr('href'),
+				diffVal = mw.util.getParamValue('diff', href);
+			if ( !href
+				|| $.inArray( diffVal, [ undefined, null, '', 0, '0', 'cur' ] ) !== -1
+				|| $(this).parent().attr( 'id' ) === 't-permalink'
+				|| ( href.indexOf( location.host ) === -1
+					&& href.indexOf( '/w/index.php' ) !== 0
+					&& href.indexOf( '/wiki/' ) !== 0
+				)
+			) {
+				return;
+			}
+			newHref = mw.config.get( 'wgScript' ) + '?diff=' + diffVal;
+			oldidVal = mw.util.getParamValue( 'oldid', href );
+			if (oldidVal) {
+				newHref += '&oldid=' + oldidVal;
+			}
+			$( this ).attr( 'href', newHref );
+		} );
 	} );
-} );
 
-}( jQuery, mediaWiki ) );
+}( mediaWiki, jQuery ) );
